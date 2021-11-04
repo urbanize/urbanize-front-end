fc88cfce8a0a15f93a27b2d607650f30c29bdc3c
valterchess <valterrekabano7@gmail.com>
2021-11-04 11:12:45 -0300

    adicionando a entidade tema e construindo o banco de dados

---
+++ back-end/urbanize/src/main/java/br/org/generation/urbanize/UrbanizeApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.urbanize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanizeApplication.class, args);
+	}
+
+}
---
+++ back-end/urbanize/src/test/java/br/org/generation/urbanize/UrbanizeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.urbanize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
