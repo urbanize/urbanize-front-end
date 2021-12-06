5cb9124232e43b7a9492f2e24de3b355da5d841e
AndreiaJM <andreia.ajms@gmail.com>
2021-12-06 14:45:34 -0300

    Comunicando componentes login e cadastro com a API

--- front-end/urbanize/src/components/pages/cadastro/CadastroUsuario.tsx
+++ front-end/urbanize/src/components/pages/cadastro/CadastroUsuario.tsx
@@ -1,29 +1,82 @@
-import React from 'react';
+import React , {useState, useEffect, ChangeEvent } from 'react';
+import { useHistory } from 'react-router-dom';
+import User from '../models/User';
+import { cadastroUsuario } from '../services/Service';
 import { Grid, Box, Typography, Button, TextField } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import './CadastroUsuario.css';
 
 function CadastroUsuario() {
+
+    let history = useHistory();
+    const [confirmarSenha,setConfirmarSenha] = useState<String>("")
+    const [user, setUser] = useState<User>(
+        {
+            id: 0,
+            nomeCompleto: '',
+            usuario: '',
+            senha: ''
+        })
+
+    const [userResult, setUserResult] = useState<User>(
+        {
+            id: 0,
+            nomeCompleto: '',
+            usuario: '',
+            senha: '',
+        })
+
+    useEffect(() => {
+        if (userResult.id != 0) {
+            history.push("/login")
+        }
+    }, [userResult])
+
+
+    function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>){
+        setConfirmarSenha(e.target.value)
+    }
+
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        if(confirmarSenha == user.senha){
+        cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult)
+        alert('Usuario cadastrado com sucesso')
+        }else{
+            alert('Dados inconsistentes. Favor verificar as informações de cadastro.')
+        }
+    }
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid item xs={6} className='imagem2'></Grid>
             <Grid item xs={6} alignItems='center'>
                 <Box paddingX={10}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos2'>Cadastrar</Typography>
-                        <TextField id='nome' label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
-                        <TextField id='confirmarSenha' label='confirmarSenha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth />
+                        <TextField value={user.nomeCompleto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nomeCompleto' label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
+                        <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal'fullWidth />
+                        <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)}id='confirmarSenha' label='confirmarSenha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth /> 
                         <Box marginTop={2} textAlign='center'>
                             <Link to='/home' className='text-decorator-none'>
                                 <Button variant='contained' className='botao2'>
                                     Cancelar
                                 </Button>
                             </Link>
+                            
                             <Button type='submit' variant='contained' className='botao2 btnCancelar'>
                                 Cadastrar
                             </Button>
+                            
                         </Box>
                     </form>
                 </Box>
--- front-end/urbanize/src/components/pages/login/Login.tsx
+++ front-end/urbanize/src/components/pages/login/Login.tsx
@@ -1,11 +1,47 @@
-import { Box } from '@mui/system'
-import React from 'react'
-import Typography from '@mui/material/Typography'
-import { Grid, Button } from '@mui/material'
-import "./Login.css";
-import { formatDiagnostic } from 'typescript';
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
+import { Link, useHistory } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { login } from '../services/Service';
+import UserLogin from '../models/UserLogin';
+import './Login.css';
 
 function Login() {
+    let history = useHistory();
+    const [token, setToken] = useLocalStorage('token');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+        )
+
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+            useEffect(()=>{
+                if(token != ''){
+                    history.push('/home')
+                }
+            }, [token])
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                await login(`/usuarios/logar`, userLogin, setToken)
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Dados do usuário inconsistentes. Erro ao logar!');
+            }
+        }
 
     return (
         <Grid container className="background">
@@ -14,24 +50,24 @@ function Login() {
                 <Box display="flex" justifyContent="center" alignItems="center" height="80vh" >
                     <Box className="card" width={270} height="50vh" borderRadius={2} marginTop={12} display="flex" justifyContent="center" alignItems="center">
                         <Box >
-                            <Typography variant="h5" color="#424242" align="center">
+                            <Typography variant="h5" align="center">
                                 Entrar
                             </Typography>
-                            <form>
+                            <form onSubmit={onSubmit}>
                                 <Box marginY={4}>
-                                    <input type="text" className="color-input"
-                                        placeholder="Email" />
+                                <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
                                 </Box>
 
                                 <Box marginY={4}>
 
-                                    <input type="password" className="color-input"
-                                        placeholder="Senha" />
+                                <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+
                                 </Box>
-                            </form>
-                            <Button variant="contained" color="secondary" className="botao">
+                                <Button type='submit' variant="contained" color="secondary" className="botao">
                                     Login
-                            </Button>  
+                                </Button> 
+                            </form>
+                            
                         </Box>
                     </Box>
                 </Box>
