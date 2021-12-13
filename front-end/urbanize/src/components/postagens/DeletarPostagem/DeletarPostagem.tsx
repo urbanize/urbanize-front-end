fb5477dcd831a6ead1c4f07e84e727625aca798a
valterchess <valterrekabano7@gmail.com>
2021-12-13 16:59:46 -0300

    Adicionando componentes de Postagem

---
+++ front-end/urbanize/src/components/postagens/DeletarPostagem/DeletarPostagem.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react'
+import {Typography, Button, Box, Card, CardActions, CardContent } from "@material-ui/core"
+import './DeletarPostagem.css';
+import { useHistory, useParams } from 'react-router-dom';
+import Postagem from '../../../models/Postagem';
+import { buscaId, deleteId } from '../../pages/services/Service';
+import { useSelector } from 'react-redux';
+
+import { toast } from 'react-toastify';
+import { UserState } from '../../../store/user/userReducer';
+
+function DeletarPostagem() {
+    let history = useHistory();
+    const { id } = useParams<{id: string}>();
+    const token = useSelector<UserState, UserState["tokens"]>(
+      (state) => state.tokens
+    );
+    const [post, setPosts] = useState<Postagem>()
+
+    useEffect(() => {
+        if (token == "") {
+          toast.error('Você precisa estar logado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+        });
+            history.push("/login")
+    
+        }
+    }, [token])
+
+    useEffect(() =>{
+        if(id !== undefined){
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/postagens/${id}`, setPosts, {
+            headers: {
+              'Authorization': token
+            }
+          })
+        }
+
+        function sim() {
+            history.push('/posts')
+            deleteId(`/postagens/${id}`, {
+              headers: {
+                'Authorization': token
+              }
+            });
+            toast.success('Postagem deletada com sucesso', {
+              position: "top-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: false,
+              draggable: false,
+              theme: "colored",
+              progress: undefined,
+          });
+          }
+        
+          function nao() {
+            history.push('/posts')
+          }
+  return (
+    <>
+      <Box m={2}>
+        <Card variant="outlined" >
+          <CardContent>
+            <Box justifyContent="center">
+              <Typography color="textSecondary" gutterBottom>
+                Deseja deletar a Postagem:
+              </Typography>
+              <Typography color="textSecondary" >
+              {post?.titulo}
+              </Typography>
+            </Box>
+
+          </CardContent>
+          <CardActions>
+            <Box display="flex" justifyContent="start" ml={1.0} mb={2} >
+              <Box mx={2}>
+              <Button onClick={sim} variant="contained" className="marginLeft" size='large' color="primary">
+                Sim
+              </Button>
+              </Box>
+              <Box>
+              <Button  onClick={nao} variant="contained" size='large' color="secondary">
+                Não
+              </Button>
+              </Box>
+            </Box>
+          </CardActions>
+        </Card>
+      </Box>
+    </>
+  );
+}
+export default DeletarPostagem;
---
+++ front-end/urbanize/src/components/postagens/listapostagem/ListaPostagem.tsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Postagem from '../../../models/Postagem';
+import { busca } from '../../pages/services/Service';
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import './ListaPostagem.css';
+import { useHistory } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { UserState } from '../../../store/user/userReducer';
+
+function ListaPostagem() {
+  const [posts, setPosts] = useState<Postagem[]>([])
+  let history = useHistory();
+
+  const token = useSelector<UserState, UserState["tokens"]>(
+    (state) => state.tokens
+  );
+  const tipo = useSelector<UserState, UserState["tipos"]>(
+    (state) => state.tipos
+  );
+
+  useEffect(() => {
+    if (token == "") {
+      toast.error('Você precisa estar logado', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,
+      });
+      history.push("/login")
+
+    }
+  }, [token])
+
+  async function getPost() {
+    await busca("/postagens", setPosts, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+  useEffect(() => {
+
+    getPost()
+
+  }, [posts.length])
+
+  var botoes: string
+  if (tipo != "Admin") {
+    botoes = "bottom-none"
+  }
+
+  return (
+    <>
+      {
+        posts.map(post => (
+          <Box m={2} >
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Postagens
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {post.titulo}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {post.texto}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {post.tema?.descricao}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" className={botoes} mb={1.5}>
+                  {/* crie uma class que recebe botoes */}
+                  <Link to={`/formularioPostagem/${post.id}`} className="text-decorator-none" >
+                    <Box mx={1}>
+                      <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarPostagem/${post.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' color="secondary">
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
+                </Box>
+              </CardActions>
+            </Card>
+          </Box>
+        ))
+      }
+    </>
+  )
+}
+
+export default ListaPostagem;
