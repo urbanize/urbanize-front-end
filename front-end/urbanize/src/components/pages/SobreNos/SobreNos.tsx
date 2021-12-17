d9e919823a1df910003c70a046649a2fcf82b2eb
AndreiaJM <andreia.ajms@gmail.com>
2021-12-17 16:46:54 -0300

    Atualização da pagina sobre nós

--- front-end/urbanize/src/components/pages/SobreNos/SobreNos.tsx
+++ front-end/urbanize/src/components/pages/SobreNos/SobreNos.tsx
@@ -21,42 +21,54 @@ function SobreNos() {
 
                     <Grid container direction="row" justifyContent="center" alignItems="center">
                         <Grid item xs={6}>
-                            <Box className='box-texto'>
+                            <Box className='box-texto' paddingRight={7} paddingLeft={7}>
+                                <Box paddingBottom={2}>
                                 <Typography variant='h3' component='h3' align='center'>
                                     O que é?
                                 </Typography>
-                                <Typography>
+                                </Box>
+                                <Box paddingBottom={2} >
+                                <Typography className='espaco-letras'>
                                     Baseada na ODS 11 – Cidades e Comunidades Sustentáveis da ONU,
                                     Criamos uma rede social com o objetivo de conectar pessoas através de um objetivo comum: dar vida e ocupar espaços públicos urbanos.
                                 </Typography>
+                                </Box>
                             </Box>
                         </Grid>
                         <Grid item xs={6}>
-                            <Box className='box-texto'>
+                            <Box className='box-texto' paddingLeft={7} paddingRight={7}>
+                                <Box paddingBottom={2}>
                                 <Typography variant='h3' component='h3' align='center'>
                                     Como?
                                 </Typography>
-                                <Typography>
+                                </Box>
+                                <Typography className='espaco-letras'>
                                     Por meio da divulgação de iniciativas individuais e de coletivos, nossa API irá conectar pessoas e/ou empresas que buscam atuar de forma ativa na promoção de revitalização e ações de resgate da qualidade de vida em parques e praças.
                                 </Typography>
                             </Box>
                         </Grid>
                         <Grid item xs={6}>
-                            <Box className='box-texto'>
+                            <Box className='box-texto' paddingRight={7} paddingLeft={7}>
+                                <Box paddingBottom={2}>
                                 <Typography variant='h3' component='h3' align='center'>
                                     Para Quem?
                                 </Typography>
-                                <Typography>
+                                </Box>
+                                <Box paddingBottom={14} >
+                                <Typography className='espaco-letras'>
                                     A Promoção de espaços públicos seguros e acessíveis é para todes, principalmente para crianças, mulheres, idosos e pessoas com deficiência.
                                 </Typography>
+                                </Box>
                             </Box>
                         </Grid>
                         <Grid item xs={6}>
-                            <Box className='box-texto'>
+                            <Box className='box-texto' paddingLeft={7} paddingRight={7}>
+                                <Box paddingBottom={2} >
                                 <Typography variant='h3' component='h3' align='center'>
-                                    Missões e Valores:
+                                    Objetivos
                                 </Typography>
-                                <Typography>
+                                </Box>
+                                <Typography className='espaco-letras'>
                                     Estímulo à participação ativa da sociedade civil em políticas públicas;
                                     Identificação de agentes locais com maior facilidade na identificação de demandas e resolução de conflitos;
                                     Colaboração entre setor privado e sociedade civil;
@@ -70,8 +82,8 @@ function SobreNos() {
                 </Grid>
                 <Grid container justifyContent="center">
                     <Grid item xs={6} >
-                        <Box >
-                            <Typography variant='h3' component='h3' align='center' className='box-texto'>
+                        <Box paddingBottom={7} >
+                            <Typography variant='h2' component='h2' align='center' className='box-texto'>
                                 Quem faz a Urbanize?
                             </Typography>
                         </Box>
@@ -79,15 +91,22 @@ function SobreNos() {
                 </Grid>
 
                 <Grid container xs={12} justifyContent='center' alignItems='center' >
-
-
-                    <Grid item xs={4}>
-                        <Box justifyContent="center" alignItems="center" >
-                            <img className='box-fotos center' src="https://i.imgur.com/sYaKCkV.png" alt="Dev Andreia" />
-
-                        </Box>
-                        <Box paddingRight={8} display="flex" alignItems="center" justifyContent="center">
-                        
+                <Box justifyContent="center" alignItems="center">
+                    <Box  justifyContent="center" alignItems="center" >
+                            <img className='box-fotos' src="https://i.imgur.com/sYaKCkV.png" alt="Dev Andreia" />
+                            </Box>
+                            <Box >
+                                <Typography variant='h5' component='h5' align='center'>
+                                    Andreia Menezes
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                    Desenvolvedora Full Stack 
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                    Estudante de Biomedicina
+                                </Typography>
+                            </Box>
+                        <Box alignItems="center" justifyContent="center"  className='box-icones' >
                             <a href=" https://github.com/AndreiaJM" target="_blank">
                                 <GitHubIcon style={{ fontSize: 50, color: "#f99e29" }} />
                             </a>
@@ -95,10 +114,26 @@ function SobreNos() {
                                 <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
                             </a>
                         </Box>
-                    </Grid>
-                    <Grid item xs={4}>
+                    
+                    </Box>
+                    <Box>
+                    <Box justifyContent="center" alignItems="center">
                         <img className='box-fotos' src="https://i.imgur.com/pu8PzOP.png" alt="Dev Carlos" />
-                        <Box paddingRight={8} display="flex" alignItems="center" justifyContent="center">
+                    </Box>
+
+                    <Box >
+                                <Typography variant='h5' component='h5' align='center'>
+                                    Carlos Botelho
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                    Desenvolvedor Full Stack 
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                    Arquiteto Urbanista
+                                </Typography>
+                            </Box>
+
+                        <Box display="flex" alignItems="center" justifyContent="center" className='box-icones' >
 
                             <a href="https://github.com/botelhocarlos" target="_blank">
                                 <GitHubIcon style={{ fontSize: 50, color: "#f99e29" }} />
@@ -107,10 +142,25 @@ function SobreNos() {
                                 <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
                             </a>
                         </Box>
-                    </Grid>
-                    <Grid item xs={4}>
+                    </Box>
+                    <Box>
+                        <Box>
                         <img className='box-fotos' src="https://i.imgur.com/dAOvWUg.png" alt="Dev Renan" />
-                        <Box paddingRight={8} display="flex" alignItems="center" justifyContent="center">
+                        </Box>
+
+                        <Box >
+                                <Typography variant='h5' component='h5' align='center'>
+                                    Renan
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                    Desenvolvedor Full Stack 
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                    Educador Fisico
+                                </Typography>
+                            </Box>
+
+                        <Box display="flex" alignItems="center" justifyContent="center" className='box-icones'>
 
                             <a href=" https://github.com/RenanMoreira92" target="_blank">
                                 <GitHubIcon style={{ fontSize: 50, color: "#f99e29" }} />
@@ -119,10 +169,26 @@ function SobreNos() {
                                 <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
                             </a>
                         </Box>
-                    </Grid>
-                    <Grid item xs={4}>
+                    </Box>
+                    
+                    <Box>
+                        <Box>
                         <img className='box-fotos' src="https://i.imgur.com/eqEohka.png" alt="Dev Thaís" />
-                        <Box paddingRight={8} display="flex" alignItems="center" justifyContent="center">
+                        </Box>
+
+                        <Box >
+                                <Typography variant='h5' component='h5' align='center'>
+                                    Thaís Oliveira
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                    Desenvolvedora Full Stack 
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                    Engenheira de Produção
+                                </Typography>
+                            </Box>
+
+                        <Box display="flex" alignItems="center" justifyContent="center" className='box-icones'>
 
                             <a href=" https://github.com/tholivera" target="_blank">
                                 <GitHubIcon style={{ fontSize: 50, color: "#f99e29" }} />
@@ -131,10 +197,25 @@ function SobreNos() {
                                 <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
                             </a>
                         </Box>
-                    </Grid>
-                    <Grid item xs={4}>
+                    </Box>
+                    <Box>
+                        <Box>
                         <img className='box-fotos' src="https://i.imgur.com/E6FvVWJ.png" alt="Dev Valter" />
-                        <Box paddingRight={8} display="flex" alignItems="center" justifyContent="center">
+                        </Box>
+
+                        <Box >
+                                <Typography variant='h5' component='h5' align='center'>
+                                    Valter Silva
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                    Desenvolvedor Full Stack 
+                                </Typography>
+                                <Typography variant='h6' component='h6' align='center'>
+                                Operador de Rastreamento
+                                </Typography>
+                            </Box>
+
+                        <Box display="flex" alignItems="center" justifyContent="center" className='box-icones'>
 
                             <a href="https://github.com/valterchess" target="_blank">
                                 <GitHubIcon style={{ fontSize: 50, color: "#f99e29" }} />
@@ -143,7 +224,8 @@ function SobreNos() {
                                 <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
                             </a>
                         </Box>
-                    </Grid>
+                    </Box>
+                
 
 
                 </Grid>
